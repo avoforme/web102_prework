6367b9446c8edd630a5290c0ab8c6d92d4d2b654
thanhavo <hophuongthanh028@gmail.com>
2024-07-16 17:24:59 +0700

    Initially display funded games

--- index.js
+++ index.js
@@ -139,6 +139,8 @@ unfundedBtn.addEventListener("click", filterUnfundedOnly);
 fundedBtn.addEventListener("click", filterFundedOnly);
 allBtn.addEventListener("click", showAllGames);
 
+filterFundedOnly()
+
 /*************************************************************************************
  * Challenge 6: Add more information at the top of the page about the company.
  * Skills used: template literals, ternary operator
